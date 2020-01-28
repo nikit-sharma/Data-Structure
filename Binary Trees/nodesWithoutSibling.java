ef109d9a3d83ae1f95468f8736df01db8c9da271
Nikit Sharma <nikitsharma798@gmail.com>
2020-01-28 22:15:11 +0530

    inital commit

---
+++ Binary Trees/nodesWithoutSibling.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+
+public class Solution {
+
+	/*	Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+		}
+	}
+	 */
+
+	public static void printNodesWithoutSibling(BinaryTreeNode<Integer> root) {
+		
+		// Write your code here
+        
+        if(root==null)
+        {
+            return;
+        }
+        
+        if(root.left==null && root.right!=null)
+        {
+            System.out.println(root.right.data);
+        }
+        
+        if(root.left!=null && root.right==null)
+        {
+            System.out.println(root.left.data);
+        }
+        
+        printNodesWithoutSibling(root.left);
+        printNodesWithoutSibling(root.right);
+      
+		
+
+	}
+
+
+
+}
